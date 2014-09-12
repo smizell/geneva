882da8edc27f83f53de1940b2a93588b1662845a
Stephen Mizell <smizell@gmail.com>
2014-09-11 22:43:06 -0500

    Add all functions back in from master

--- test/base_test.js
+++ test/base_test.js
@@ -102,6 +102,14 @@ describe("Geneva Core", function() {
   });
 
   describe("base", function() {
+    describe("def", function() {
+      it("should set a stored variable", function() {
+        geneva.run(["!def", "test", "value"]);
+        var testVar = geneva.get("test");
+        expect(testVar.value).to.equal("value");
+      });
+    });
+
     describe("array", function() {
       it("should return a normal array", function() {
         var val = geneva.run([1, 2, 3]);
@@ -119,6 +127,112 @@ describe("Geneva Core", function() {
         geneva.set("test", "var", 4)
         var identity = geneva.run(["!identity", "~test"]);
         expect(identity).to.equal(4);
+      });
+    });
+  });
+
+  describe("math", function() {
+    describe("inc", function() {
+      it("should increment a number by one", function() {
+        var two = geneva.run(["!inc", 1]);
+        expect(two).to.equal(2);
+      });
+    });
+
+    describe("+", function() {
+      it("should add two values", function() {
+        var five = geneva.run(["!+", 2, 3]);
+        expect(five).to.equal(5);
+      });
+
+      it("should add multiple values", function() {
+        var ten = geneva.run(["!+", 2, 3, 5]);
+        expect(ten).to.equal(10);
+      });
+    });
+
+    describe("-", function() {
+      it("should subtract two values", function() {
+        var five = geneva.run(["!-", 10, 5]);
+        expect(five).to.equal(5);
+      });
+
+      it("should subtract multiple values", function() {
+        var three = geneva.run(["!-", 10, 5, 2]);
+        expect(three).to.equal(3);
+      });
+    });
+  });
+
+  describe("logic", function() {
+    describe("=", function() {
+      it("should return true for equal values", function() {
+        var equal = geneva.run(["!=", 5, 5]);
+        expect(equal).to.be.true
+      });
+
+      it("should return true for multiple equal values", function() {
+        var equal = geneva.run(["!=", 5, 5, 5, 5]);
+        expect(equal).to.be.true
+      });
+
+      it("should return false for non-equal values", function() {
+        var equal = geneva.run(["!=", 5, 4]);
+        expect(equal).to.be.false
+      });
+    });
+
+    describe("not=", function() {
+      it("should return false for equal values", function() {
+        var equal = geneva.run(["!not=", 5, 5]);
+        expect(equal).to.be.false;
+      });
+
+      it("should return false for multiple equal values", function() {
+        var equal = geneva.run(["!not=", 5, 5, 5, 5]);
+        expect(equal).to.be.false;
+      });
+
+      it("should return true for non-equal values", function() {
+        var equal = geneva.run(["!not=", 5, 4]);
+        expect(equal).to.be.true;
+      });
+    });
+
+    describe("if", function() {
+      describe("when else is not given", function() {
+        it("should return value if passes", function() {
+          var passes = geneva.run(["!if", true, "OK"]);
+          expect(passes).to.equal("OK");
+        });
+
+        it("should return value if fails", function() {
+          var fails = geneva.run(["!if", false, "OK"]);
+          expect(fails).to.equal(null);
+        });
+      });
+
+      describe("when else is given and fails", function() {
+        it("should return the else value", function() {
+          var fails = geneva.run(["!if", false, "OK", "FAIL"]);
+          expect(fails).to.equal("FAIL")
+        });
+      });
+    });
+  });
+
+  describe("functional", function() {
+    describe("map", function() {
+      it("should map to a function", function() {
+        var inc = geneva.run(["!map", "~inc", [1, 2, 3]]);
+        expect(inc).to.eql([2, 3, 4]);
+      });
+    });
+
+    describe("reduce", function() {
+      it("should reduce to a function", function() {
+        var sum = geneva.run(["!reduce", "~+", [1, 2, 3]]);
+        expect(sum).to.eql(6);
       });
     });
   });
