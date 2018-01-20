cff9371514acce22de67a73bbf46d7284e987332
Stephen Mizell <smizell@gmail.com>
2018-01-19 21:16:55 -0600

    Code cleanup

--- test/test_ref.js
+++ test/test_ref.js
@@ -2,17 +2,15 @@ const Geneva = require('../lib/base');
 const chai = require('chai');
 const expect = chai.expect;
 
-describe('Geneva', () => {
-  describe('references', () => {
-    context('when adding a global reference', () => {
-      it('can be referenced in a function call', () => {
-        const geneva = new Geneva();
-        const result = geneva.run(
-          ['!do',
-            ['!def', 'x', 42],
-            ['!identity', '~x']]);
-        expect(result).to.equal(42);
-      });
+describe('References', () => {
+  context('when adding a global reference', () => {
+    it('can be referenced in a function call', () => {
+      const geneva = new Geneva();
+      const result = geneva.run(
+        ['!do',
+          ['!def', 'x', 42],
+          ['!identity', '~x']]);
+      expect(result).to.equal(42);
     });
   });
 });
