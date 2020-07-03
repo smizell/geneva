51a218e9c83a37e8fad79fe2e94a03ec90cf34d4
Stephen Mizell <smizell@gmail.com>
2020-07-03 15:27:27 -0500

    Add config builder

---
+++ test/test_config_builder.js
@@ -0,0 +1,21 @@
+const { ConfigBuilder } = require("../lib/base");
+const chai = require("chai");
+const expect = chai.expect;
+const fs = require("fs");
+
+describe("ConfigBuilder", function () {
+  context("basic configuration", function () {
+    let config;
+
+    beforeEach(function () {
+      config = ConfigBuilder.fromYAML(
+        fs.readFileSync("./test/examples/basic-config.yml", "utf-8")
+      );
+    });
+
+    it("correctly executes", function () {
+      const results = config.run({ firstName: "Jane", lastName: "Doe" });
+      expect(results.greeting).to.equal("Hello, Jane Doe");
+    });
+  });
+});
